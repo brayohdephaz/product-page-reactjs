060fde402a0941fcaa1957954a7ca1e23085c8ea
brayohdephaz <bm.mutwiri@gmail.com>
2021-04-02 09:38:21 +0300

    dynamically rendered more components and rearraged files

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import React, { Component } from "react";
 
 import classes from "./App.module.css"; //We need to create a name so we may reference when calling the styles from the modeule.css
 
-import ProductData from "./ProductData";
-import ProductPreview from "./ProductPreview";
-import ProductDetailsComp from "./ProductDetailsComp";
-import TopbarComp from "./TopbarComp";
+import ProductData from "./Utilities/ProductData";
+import ProductPreview from "./Components/ProductPreview/ProductPreview";
+import ProductDetailsComp from "./Components/ProductDetails/ProductDetailsComp";
+import TopbarComp from "./Components/ToolBar/TopbarComp";
 
 // We will make use of state methods and therefore we will change our function to class
 class App extends Component {
   state = {
     ProductData: ProductData, //We setting a new state and setting it to our object from  Product data
+    currentImagePriview: "https://imgur.com/PTgQlim.png", //Defined our image here at the state
+    showHeartBeat: true,
+    curretHour:
+      new Date().getHours() > 9
+        ? new Date().getHours()
+        : "0" + new Date().getHours(),
+    curretMinute:
+      new Date().getMinutes() > 9
+        ? new Date().getMinutes()
+        : "0" + new Date().getMinutes(),
   };
+
   render() {
     return (
       <div className="App">
@@ -22,7 +33,12 @@ class App extends Component {
         <div className={classes.MainContainer}>
           {/* Product preview */}
           <div className={classes.ProductPreview}>
-            <ProductPreview />
+            <ProductPreview
+              currentImagePriview={this.state.currentImagePriview}
+              showHeartBeat={this.state.showHeartBeat}
+              curretHour={this.state.curretHour}
+              curretMinute={this.state.curretMinute}
+            />
           </div>
           <div className={classes.ProductData}>
             <ProductDetailsComp data={this.state.ProductData} />
